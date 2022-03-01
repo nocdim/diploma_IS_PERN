a58d6c369549613d61a5d02e1cfb15405e429a97
nocdim <nokkudimu@gmail.com>
2022-03-02 00:36:44 +0300

    created skeletons of controllers and routers

---
+++ server/controllers/productController.js
@@ -0,0 +1,13 @@
+class ProductController {
+    async create(req, res) {
+
+    }
+    async getAll(req, res) {
+        
+    }
+    async getOne(req, res) {
+        
+    }
+}
+
+module.exports = new ProductController()
