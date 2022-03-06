50441d1feb08ed09c45fcf05936a5bd966070877
nocdim <nokkudimu@gmail.com>
2022-03-06 17:20:42 +0300

    frontend organized

--- server/routes/brandRouter.js
+++ server/routes/brandRouter.js
@@ -3,8 +3,6 @@ const router = new Router()
 const brandController = require('../controllers/brandController')
 const checkRole = require('../middleware/checkRoleMiddleware')
 
-
-
 router.post('/', checkRole('ADMIN'), brandController.create)
 router.get('/', brandController.getAll)
 
