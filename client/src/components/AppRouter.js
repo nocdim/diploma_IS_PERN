64e7299f5257636560cd5d8886e2965f6eff1266
nocdim <nokkudimu@gmail.com>
2022-03-06 21:46:58 +0300

    rename

--- client/src/components/AppRouter.js
+++ client/src/components/AppRouter.js
@@ -1,17 +1,18 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import {Routes, Route} from 'react-router-dom'
 import { authRoutes, publicRoutes } from '../routes'
-
+import { Context } from '../index'
 
 const AppRouter = () => {
-    const isAuth = false
+    const { user } = useContext(Context)
+    console.log(user)
     return (
         <Routes>
-            {isAuth && authRoutes.map(({path, element}) => 
-                <Route exact path={path} element={element}/>
+            {user.isAuth && authRoutes.map(({path, element}) => 
+                <Route key={path} exact path={path} element={element}/>
             )}
              {publicRoutes.map(({path, element}) => 
-                <Route exact path={path} element={element}/>
+                <Route key={path} exact path={path} element={element}/>
             )}
         </Routes>
     )
