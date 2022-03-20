c86e8e0f2d58560de8bb94dace87c278c3aa70d1
nocdim <nokkudimu@gmail.com>
2022-03-20 15:44:33 +0300

    useNavigate hook implemented

--- client/src/components/ProductItem.jsx
+++ client/src/components/ProductItem.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import { Card, Col, Image } from "react-bootstrap"
 import star from "../assets/star.png";
+import { useNavigate } from  "react-router-dom"
+import { PRODUCT_ROUTE } from '../utils/consts';
 
 const ProductItem = ({ product }) => {
+    const navigate = useNavigate()
+    console.log(navigate)
     return (
-        <Col md={3} className={"mt-3"}>
+        <Col md={3} className={"mt-3"} onClick={() => navigate(PRODUCT_ROUTE + '/' + product.id)}>
             <Card style={{ width: 150, cursor: 'pointer' }} border={"light"}>
                 <Image width={150} height={150} src={product.img} />
                 <div className="text-black-50 mt-1 d-flex justify-content-between align-items-center">
