16a15f9dc0fd436cbcdc2b78da519b29c82dac05
nocdim <nokkudimu@gmail.com>
2022-04-12 16:58:08 +0300

    addtype reworked

--- client/src/components/modals/CreateType.jsx
+++ client/src/components/modals/CreateType.jsx
@@ -1,17 +1,47 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { Modal, Button, Form } from "react-bootstrap"
 import { createType } from "../../http/productAPI";
+import { observer } from "mobx-react-lite";
+import "../../styles/admin.css"
 
-const CreateType = ({show, onHide}) => {
+const CreateType = observer(({ show, onHide }) => {
 
-    const [file, setFile] = useState(null)
     const [type, setType] = useState('')
+    const [image, setImage] = useState(null)
+    const [preview, setPreview] = useState('')
+    const fileInputRef = useRef()
+
+    const imgUpload = (event) => {
+        event.preventDefault()
+        fileInputRef.current.click()
+    }
+
+    const selectImage = event => {
+        const file = event.target.files[0]
+        if (file && file.type.substr(0, 5) === "image") {
+            setImage(file)
+        } else {
+            setImage(null)
+        }
+    }
+
+    useEffect(() => {
+        if (image) {
+            const reader = new FileReader()
+            reader.onloadend = () => {
+                setPreview(reader.result)
+            }
+            reader.readAsDataURL(image)
+        } else {
+            setPreview('')
+        }
+    }, [image])
 
     const addType = async () => {
         try {
             const formData = new FormData()
             formData.append('name', type)
-            formData.append('img', file)
+            formData.append('img', image)
             await createType(formData).then(data => {
                 setType('')
                 onHide()
@@ -22,15 +52,11 @@ const CreateType = ({show, onHide}) => {
         }
     }
 
-    const selectFile = e => {
-        setFile(e.target.files[0])
-    }
-
     return (
         <Modal
             show={show}
             onHide={onHide}
-            size="lg"
+            size="sm"
             centered
         >
             <Modal.Header closeButton>
@@ -40,16 +66,35 @@ const CreateType = ({show, onHide}) => {
             </Modal.Header>
             <Modal.Body>
                 <Form>
+                    <div className="d-flex justify-content-center mb-3">
+                    {preview ?
+                        <img
+                            className="img"
+                            src={preview}
+                            alt={preview}
+                            onClick={imgUpload}
+                        />
+                        :
+                        <button
+                            className="imgButton"
+                            onClick={imgUpload}
+                        >
+                            Загрузите изображение
+                        </button>
+                    }
+                    <Form.Control
+                        type="file"
+                        accept="image/*"
+                        style={{ display: 'none' }}
+                        ref={fileInputRef}
+                        onChange={selectImage}
+                    />
+                    </div>
+                    
                     <Form.Control
                         value={type}
                         onChange={e => setType(e.target.value)}
                         placeholder={"Введите название раздела"}
-                    />
-                    <Form.Control
-                        className="mt-3"
-                        type="file"
-                        accept="image/*"
-                        onChange={selectFile}
                     />
                 </Form>
             </Modal.Body>
@@ -59,6 +104,6 @@ const CreateType = ({show, onHide}) => {
             </Modal.Footer>
         </Modal>
     )
-}
+})
 
 export default CreateType
